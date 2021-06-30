46fc9e30ca6229e30988091e78eaa0582432c09e
Ozkan Pakdil <ozkan.pakdil@gmail.com>
2021-06-30 13:47:11 +0100

    cleanup

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ var apicache = require('apicache')
 var cache = apicache.middleware
 const net = require('net')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 var whois = require('whois')
 var counter = 0;
-
-var whoisoptions = {
-    "proxy": {
-        "host": "localhost",
-        "port": 9050,
-        "type": 5    // or 4
-    }
-};
 
 app.get('/whois', cache('48 hours'), (req, res) => {
     console.log(++counter + "-" + req.query.ip)
